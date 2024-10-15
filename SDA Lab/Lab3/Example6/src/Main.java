732fbc71421ba685517fbb88327abce0e764c124
“WaliMuhammadAhmad” <2021se39@student.uet.edu.pk>
2024-10-15 21:53:23 +0500

    Example 6/7 fixed

--- SDA Lab/Lab3/Example6/src/Main.java
+++ SDA Lab/Lab3/Example6/src/Main.java
@@ -4,16 +4,5 @@ public class Main {
         Calculator cal = new Calculator();
         System.out.println(cal.add(12,8));
         System.out.println(cal.add(123456789,987654314));
-
-        ////////////////////////////
-        System.out.println();
-        ////////////////////////////
-        Animal animal = new Animal();
-        animal.sound();
-        Dog dog = new Dog();
-        dog.sound();
-        //Overloading
-        Animal newDog = new Dog();
-        newDog.sound();
     }
 }
